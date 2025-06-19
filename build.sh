fa63aff2dc699e291e85b345592bf0008d764d2d
HGNEO <130890333+HGNEO@users.noreply.github.com>
2025-06-20 00:13:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
+repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs; \
 /opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/efeisot/android_device_xiaomi_munch device/xiaomi/munch; \
-git clone https://github.com/efeisot/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
+git clone https://github.com/hgneo/android_device_xiaomi_munch device/xiaomi/munch; \
+git clone https://github.com/hgneo/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
