b0590d4683e29681ad5c54c7c082a846f7140caf
THEZORTHIX <yozare69@gmail.com>
2025-06-20 20:56:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,4 +21,4 @@ git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/app
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 . build/envsetup.sh; \
 lunch derp_munch-bp1a-userdebug
-make derp
+mka derp
